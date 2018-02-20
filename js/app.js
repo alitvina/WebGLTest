c9bbf275d02d26c7e2d40e81fd9b43bd08a79279
Anastasiia Litvina <al@click-solutions.de>
2018-02-20 11:15:41 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,192 @@
+// window.onload = function() {
+
+  main();
+
+  function main() {
+    const canvas = document.querySelector("#canvas");
+    // initialize GL context
+    const gl = canvas.getContext("webgl");
+
+    // Only continue if WebGl is available and working
+    if(!gl) {
+      alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+      return;
+    }
+
+    // Vertex shader program
+    const vsSource = `
+      attribute vec4 aVertexPosition;
+
+      uniform mat4 uModelViewMatrix;
+      uniform mat4 uProjectionMatrix;
+
+      void main() {
+        gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+      }
+    `;
+
+    // Fragment shader program
+    const fsSource = `
+      void main() {
+        gl_FragColor = vec4(0.7, 0.8, 0.1, 1.0);
+      }
+    `;
+
+    // Initialize a shader program
+    const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+
+    // Collect all the info for shader program
+    const programInfo = {
+      program: shaderProgram,
+      attribLocations: {
+        vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+      },
+      uniformLocations: {
+        projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+        modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+      }
+    };
+
+    // Build all objects
+    const buffers = initBuffers(gl);
+
+    // Draw scene
+    drawScene(gl, programInfo, buffers);
+  }
+
+  function initBuffers(gl) {
+    // Create a buffer for the square's positions
+    const positionBuffer = gl.createBuffer();
+
+    // Select the positionBuffer as the one to apply buffer operations to from here out
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // Create an array of position for the square
+
+    const positions = [
+       1.0,  1.0,
+      -1.0,  1.0,
+       1.0, -1.0,
+      -1.0, -1.0,
+    ];
+
+    // Pass the list of positions into WebGL to build the shape.
+    // By creating a Float32Array from the JavaScript array, then use to fill the current buffer
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    return {
+      position: positionBuffer,
+    };
+  }
+  function drawScene(gl, programInfo, buffers) {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);    // Clear to black, full opaque
+    gl.clearDepth(1.0)                    // Clear everything
+    gl.enable(gl.DEPTH_TEST);             // Enable depth testing
+    gl.depthFunc(gl.LEQUAL);              // Near things obscure far things
+
+    // Clear the canvas before drawing
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Create a perspective matrix. Used to simulate the distortion of perspective in a camera
+    const fieldOfView = 45 * Math.PI / 180; // in radians
+    const aspect = gl.canvas.clientWidth / gl.canvas.clienHeight;
+    const zNear = 0.1;
+    const zFar = 100.0;
+    const projectionMatrix = mat4.create();
+
+    mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
+
+    // Set the drawing position to the "Identity" point (center of the scene)
+    const modelViewMatrix = mat4.create();
+
+    // Move the drawing position
+    mat4.translate(modelViewMatrix,         // destination matrix
+                  modelViewMatrix,          // matrix to translate
+                  [-0.0, 0.0, -6.0]);       // ammount to translate
+
+    // Tell WebGL how to pull out the positions from the position buffer intp vertexPosition attribute
+    {
+      const numComponents = 2; //pull out 2 values per iteration
+      const type = gl.FLOAT; //the data in buffer is 32bit floats
+      const normalize = false; // don't normalize
+      const stride = 0; // how many bytes to get from one set of values to next. 0 = use type and numComponents
+      const offset = 0; // how many bytes inside the buffer to start from;
+
+      gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+      gl.vertexAttribPointer(
+        programInfo.attribLocations.vertexPosition,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset);
+      gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+    }
+
+    // Tell WebGL to use our program when drawing
+    gl.useProgram(programInfo.program);
+
+    //Set the shader uniforms
+    gl.uniformMatrix4fv(
+      programInfo.uniformLocations.projectionMatrix,
+      false,
+      projectionMatrix);
+    gl.uniformMatrix4fv(
+      programInfo.uniformLocations.modelViewMatrix,
+      false,
+      modelViewMatrix);
+
+    {
+      const offset = 0;
+      const vertexCount = 4;
+      gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+    }
+  }
+
+
+  //
+  // Initialize a shader program
+  //
+  function initShaderProgram(gl, vsSource, fsSource) {
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+    // Create the shader program
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    // If creating shader program failed
+    if(!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+      alert("Unable to initialize the shader program: " + gl.getProgramInfoLog(shaderProgram));
+      return null;
+    }
+
+    return shaderProgram;
+  }
+
+  //
+  // Create a shader of given type, upload the source and compile it
+  //
+  function loadShader(gl, type, source) {
+    const shader = gl.createShader(type);
+
+    // Send the source to the shader object
+    gl.shaderSource(shader, source);
+
+    // Compile the shader program
+    gl.compileShader(shader);
+
+    // See if compiled successfully
+    if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      alert("An error has occured compiling the shaders: " + gl.getShaderInfoLog(shader));
+      gl.deleteShader(shader);
+      return null;
+    }
+
+    return shader;
+  }
+
+
+// }
